f64ca1c89d4e5e2e4ffc196616e445f50b141f5e
Fredrik Christensen <fredrik.christensen@no.experis.com>
2024-06-21 13:28:04 +0200

    Fix for generating relative paths instead of absolutes...

---
+++ fix-paths.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const buildDir = path.join(__dirname, 'build');
+
+const indexPath = path.join(buildDir, 'index.html');
+let indexContent = fs.readFileSync(indexPath, 'utf8');
+
+// Replace absolute paths with relative paths
+indexContent = indexContent.replace(/\/chatterbox\//g, './');
+
+// Write the changes back to the file
+fs.writeFileSync(indexPath, indexContent, 'utf8');
